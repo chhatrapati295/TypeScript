82d9e1c3c63b6e7cfaa972ffcc515cc7e99873e7
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2024-02-04 14:44:16 +0000

    Generics example

--- file.ts
+++ file.ts
@@ -1,9 +1,6 @@
-const var1 = "Chhatrapati";
-const var2 = 23;
+type T = number;
+const myFunc = <T>(a: T): T => {
+  return a;
+};
 
-function printMyInfo(var1:string , var2:number){
-    console.log(`My name is ${var1}`);
-    
-}
-
-printMyInfo(var1 , var2)
+const ans = myFunc<T>(2);
